1acfaa9d6e5dc3ab2d17c58e358c2275246b3286
Attila Szasz <attila.szasz@iquestgroup.com>
2016-09-29 12:40:58 +0300

    Cleanup dependencies

--- src/main/java/ro/szzsa/livescore/server/service/impl/NotificationServiceImpl.java
+++ src/main/java/ro/szzsa/livescore/server/service/impl/NotificationServiceImpl.java
@@ -4,12 +4,13 @@ import org.apache.log4j.Logger;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import ro.szzsa.livescore.api.device.protocol.notification.NotificationConstants;
 import ro.szzsa.livescore.model.Game;
 import ro.szzsa.livescore.server.model.notification.Data;
 import ro.szzsa.livescore.server.model.notification.Notification;
 import ro.szzsa.livescore.server.service.NotificationService;
 import ro.szzsa.utils.connector.Connector;
-import ro.szzsa.utils.connector.Connectors;
+import ro.szzsa.utils.connector.HttpConnectorBuilder;
 import ro.szzsa.utils.connector.Request;
 import ro.szzsa.utils.connector.exception.ConnectorException;
 import ro.szzsa.utils.converter.Converter;
@@ -23,7 +24,7 @@ public class NotificationServiceImpl implements NotificationService {
 
   private static final String FCM_SEND_URL = "https://fcm.googleapis.com/fcm/send";
 
-  private static final String STATS_TOPIC = "/topics/test";
+  private static final String STATS_TOPIC = "/topics/" + NotificationConstants.STATS_TOPIC_NAME;
 
   private static final String API_KEY = "AIzaSyB3NhdQSWGcOHn0CSAfip4h11AcXjw95hM";
 
@@ -35,7 +36,7 @@ public class NotificationServiceImpl implements NotificationService {
 
   public NotificationServiceImpl() {
     this.converter = Converters.createJsonConverter();
-    this.connector = Connectors.withApiKey(API_KEY).build();
+    this.connector = new HttpConnectorBuilder(API_KEY).build();
   }
 
   @Async
